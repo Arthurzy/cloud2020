afe4425cf1fff31bf786a1b641dd722b0bbe6c4a
Arthurzy <Arthurzy@users.noreply.github..com>
2021-01-04 18:44:16 +0800

    init

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Arthurzy
+ * @version 1.0
+ * @description: TODO
+ * @create 2021/1/4 0:04
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}

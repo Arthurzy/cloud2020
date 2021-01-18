7e170c8444b180d344f05fe2d830bc08b922f8d0
Arthurzy <Arthurzy@users.noreply.github..com>
2021-01-18 21:46:36 +0800

    untrack some files

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -47,9 +47,10 @@ public class PaymentController {
             return new CommonResult<Payment>(444, "没有对应记录，查询ID: " + id);
         }
     }
-    
-    @GetMapping("payment/lb")
-    public String getPaymentLB() {
+
+
+    @GetMapping("/payment/lb")
+    public String getPaymentLB(){
         return serverPort;
     }
 }

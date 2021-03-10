9184e1696429b4702f91d29d07bf42e678ce4ad1
Yong Zhang <yzhangcd@cn.ibm.com>
2021-03-10 18:00:06 +0800

    update

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQMain8802.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YongZhang
+ * @version 1.0
+ * @description: StreamMQMain8802
+ * @create Mar 10, 2021 5:35:03 PM
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
+
+
--- cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/impl/MessageproviderImpl.java
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/impl/MessageproviderImpl.java
@@ -2,6 +2,8 @@ package com.atguigu.springcloud.service.impl;
 
 import java.util.UUID;
 
+import javax.annotation.Resource;
+
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.MessageChannel;
@@ -18,14 +20,16 @@ import com.atguigu.springcloud.service.IMessageProvider;
 @EnableBinding(Source.class ) // 定义消息的推送管道
 public class MessageproviderImpl implements IMessageProvider{
 
+    @Resource
     private MessageChannel output; // 消息发送管道
     
     @Override
     public String send() {
         String serial = UUID.randomUUID().toString();
+        
         output.send(MessageBuilder.withPayload(serial).build());
         System.out.println("*****serial: " + serial);
-        return null;
+        return "*****serial: " + serial;
     }
 
 }

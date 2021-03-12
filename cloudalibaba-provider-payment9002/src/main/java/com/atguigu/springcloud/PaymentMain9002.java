83ea72e74ae541a8be60e72a426ce008f67531bd
Yong Zhang <yzhangcd@cn.ibm.com>
2021-03-12 17:03:48 +0800

    add nacos

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/PaymentMain9002.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author YongZhang
+ * @version 1.0
+ * @description: PaymentMain9001
+ * @create Mar 12, 2021 3:03:01 PM
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(PaymentMain9002.class, args);
+    }
+
+}
+
+
---
+++ cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author YongZhang
+ * @version 1.0
+ * @description: PaymentController
+ * @create Mar 12, 2021 3:05:35 PM
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+    
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "Hello Nacos Discovery " + serverPort + "\t id = " + id;
+    }
+}
+
+

4b29e751a12deb4f12427b9e046716b06e972469
Yong Zhang <yzhangcd@cn.ibm.com>
2021-03-23 16:24:17 +0800

    add seata 0.9.0 zip

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,11 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author YongZhang
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @create Mar 16, 2021 5:00:43 PM
  */
 @RestController
+@Slf4j
 public class FlowLimitController {
         
     @GetMapping("/testA")
@@ -19,7 +24,20 @@ public class FlowLimitController {
     
     @GetMapping("/testB")
     public String testB() {
+        log.info(Thread.currentThread().getName() + "\t" + "-------testB"); // 测试排队
         return "------------testB()";
+    }
+    
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        log.info("testD 测试RT");
+        return "------------testD()";
     }
 
 }
